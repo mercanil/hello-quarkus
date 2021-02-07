484b328805535e8a9df10f30147510ccecc7feed
mercanil <mercanil@gmail.com>
2021-02-07 21:06:41 +0000

    - added repository

---
+++ src/main/java/com/mercan/anil/Fruit.java
@@ -0,0 +1,16 @@
+package com.mercan.anil;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+import java.util.List;
+
+@Entity
+public class Fruit extends PanacheEntity {
+    public String name;
+    public String season;
+
+    public static List<Fruit> getBySeason(String season){
+        return find("season" , season).list();
+    }
+}
