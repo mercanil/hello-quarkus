200d39c1b4b4bb07d03b3e43b1305cba3252d553
mercanil <mercanil@gmail.com>
2021-02-14 18:05:04 +0000

    - Lombok dependency is removed

--- src/main/java/com/mercan/anil/customer/Customer.java
+++ src/main/java/com/mercan/anil/customer/Customer.java
@@ -1,21 +1,40 @@
 package com.mercan.anil.customer;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@RegisterForReflection
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Customer {
     private Integer id;
     private String name ;
     private String surname;
 
+
+    public Customer() {
+    }
+
     public Customer(String name, String surname) {
         this.name = name;
+        this.surname = surname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
         this.surname = surname;
     }
 }
